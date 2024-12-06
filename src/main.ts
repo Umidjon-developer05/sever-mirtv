b90fb35e66de65b122dcd4dfb6f7c21cf3c2ad71
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-12-06 14:21:10 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import { AppModule } from './app/app.module';
 async function nest() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [ 'https://www.mirtv.uz'],
-  });
+  // Barcha saytga ruxsat berish
+  app.enableCors({ origin: true });
 
   await app.setGlobalPrefix('api').listen(process.env.PORT || 8080);
 }
