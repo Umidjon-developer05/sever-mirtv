62c3202bcebed91521d37156f58654fc6d94f212
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-12-06 15:47:55 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app/app.module';
 
 async function nest() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'https://mir-tv.vercel.app' });
+  app.enableCors({
+  origin: ['https://mir-tv.vercel.app', 'https://www.mirtv.uz'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
   await app.setGlobalPrefix('api').listen(process.env.PORT || 8080);
 }
 nest();
