ac974b3005e965c4f20e3d53633a567fd9f89e3c
Umidjon-developer05 <umidjongafforov175@gmail.com>
2025-09-16 11:47:33 +0500

    dist

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
-const module_1 = require("./module");
+const app_module_1 = require("./app/app.module");
 async function nest() {
-    const app = await core_1.NestFactory.create(module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({ origin: '*' });
     await app.setGlobalPrefix('api').listen(process.env.PORT || 8080);
 }
