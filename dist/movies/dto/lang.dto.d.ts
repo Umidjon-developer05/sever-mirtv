040028efc5a5918f615b5bc1435dfaa4b3ab9ce8
Umidjon-developer05 <umidjongafforov175@gmail.com>
2025-09-16 11:33:43 +0500

    dist

--- dist/movies/dto/lang.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class LangDto {
-    uz: string;
-    ru: string;
-    en: string;
-}
--- dist/movies/dto/movie.dto.d.ts
+++
@@ -1,6 +0,0 @@
-import { LangDto } from './lang.dto';
-export declare class MovieDto {
-    '2160p': LangDto | null;
-    '1080p': LangDto | null;
-    '720p': LangDto | null;
-}
